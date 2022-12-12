948e209cc17567ac0b0ad97bb7a086ba1e1d412a
Bonheur Bushiri <bonheurbushiri@gmail.com>
2022-12-12 15:12:13 +0200

    script: function start

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ let workTime = 25;
 let breakTime = 5;
 
 let seconds = "00"
+
+function start() {
+    // change button
+    document.getElementById('start').style.display = "none";
+    document.getElementById('reset').style.display = "block";
+
+    // change the time
+    seconds = 59;
+
+    let workMinutes = workTime - 1;
+    let breakMinutes = breakTime - 1;
+
+    breakCount = 0;
